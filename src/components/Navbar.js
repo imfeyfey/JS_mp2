b3a7be8f947516bb74a55f4dbae72b341043f832
Fatima Joy Austria <130661073+imfeyfey@users.noreply.github.com>
2023-06-15 20:06:02 +0800

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+
+class Navbar extends Component {
+  state = { clicked: false };
+  handleclick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <>
+        <nav>
+          <a href="index.html" class="logo">
+            BABY E-Konsulta SYSTEM
+          </a>
+          <div>
+            <ul
+              id="navbar"
+              className={this.state.clicked ? "#navbar active" : "#navbar"}
+            >
+              <li>
+                <a class="active" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="index.html">About</a>
+              </li>
+              <li>
+                <a href="index.html">Services</a>
+              </li>
+              <li>
+                <a href="index.html">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div id="mobile" onClick={this.handleclick}>
+            <i
+              id="bar"
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
+export default Navbar;
